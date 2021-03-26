9a2387fe7fd8b3a0d8eff9b6ec3c6f766ff6e70f
aeeux <AndreasEriksen99@hotmail.com>
2021-03-26 08:36:23 +0100

    update _app.js and login.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
+import Login from './login';
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
+
+  if (!user) return <Login />
 
   return <Component {...pageProps} />
 }
---
+++ pages/login.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+function Login() {
+    return (
+        <Container>
+            <Head>
+                <title>Login</title>
+            </Head>
+        </Container>
+    )
+}
+
+export default Login
+
+const Container = styled.div`
+
+    `;
