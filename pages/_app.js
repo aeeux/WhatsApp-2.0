af90318a3bfcbb897d542d8822faa1de6d9ba7da
aeeux <AndreasEriksen99@hotmail.com>
2021-03-26 08:27:13 +0100

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebase';
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth)
 
   return <Component {...pageProps} />
 }
