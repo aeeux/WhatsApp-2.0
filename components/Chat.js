20a9633788c66e470563d72361044177ef749a7f
aeeux <AndreasEriksen99@hotmail.com>
2021-03-26 23:44:52 +0100

    added utils, extra chat component and photoURL grabber for Chat + Sidebar.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,26 @@
 import { Avatar } from '@material-ui/core';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
+import { auth, db } from '../firebase';
+import getRecipientEmail from '../utils/getRecipientEmail';
 
 function Chat({ id, users}) {
+    const [user] = useAuthState(auth);
+    const [recipientSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipientEmail(users, user)))
+
+    const recipient = recipientSnapshot?.docs?.[0]?.data();
+    const recipientEmail = getRecipientEmail(users, user);
+
     return (
         <Container>
-            <UserAvatar />
-            <p>Recipient Email</p>
+            {recipient ? (
+                <UserAvatar src={recipient?.photoURL} />
+            
+            ) : (
+                <UserAvatar>{recipientEmail[0]}</UserAvatar>
+            )}
+            <p>{recipientEmail}</p>
         </Container>
     );
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -44,7 +44,7 @@ function Sidebar() {
     return (
         <Container>
             <Header>
-                <UserAvatar onClick={() => auth.signOut()} />
+                <UserAvatar src={user.photoURL} onClick={() => auth.signOut()} />
 
                 <IconsContainer>
 
@@ -69,7 +69,7 @@ function Sidebar() {
 
             {/* List of Chats */}
             {chatsSnapshot?.docs.map(chat => (
-                <Chat key={chat.id} id={chat.id} user={chat.data().users}/>
+                <Chat key={chat.id} id={chat.id} users={chat.data().users}/>
             ))}
         </Container>
     );
