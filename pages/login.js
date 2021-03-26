2545bdab58f435b8c37a30f5385d9e524878254d
aeeux <AndreasEriksen99@hotmail.com>
2021-03-26 08:50:14 +0100

    update login.js

--- pages/login.js
+++ pages/login.js
@@ -23,11 +23,17 @@ const Container = styled.div`
         display: grid;
         place-items: center;
         height: 100vh;
+        background-color: whitesmoke;
     `;
 
 const LoginContainer = styled.div`
+        padding: 100px;
         display: flex;
         flex-direction: column;
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        box-shadow: 0px 4px 14px -3px rgba(0, 0, .7)
     `;
 
 const Logo = styled.img`
